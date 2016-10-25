f6c36696b4b4c5de0f140cd86112f6571becfa67
PauloGaldo <hades_54st@hotmail.com>
2016-10-25 01:27:53 -0300

    commit inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name travelApp
+ * @description
+ * # travelApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('travelApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngSanitize'
+  ]);
