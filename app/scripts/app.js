989a2010b0861aae3166034ff17f7488b4e8fc01
PauloGaldo <hades_54st@hotmail.com>
2016-10-26 02:05:15 -0300

    dependencias agregadas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,33 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc overview
- * @name travelApp
- * @description
- * # travelApp
- *
- * Main module of the application.
- */
-angular
-  .module('travelApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngMessages',
-    'ngSanitize'
-  ]);
+
+  /**
+   * @ngdoc overview
+   * @name travelApp
+   * @description
+   * # travelApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('travelApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngMessages',
+      'ngSanitize',
+      'ui.router',
+      'anim-in-out',
+      'ngMessages',
+      'toaster',
+      'ngCookies',
+      'LocalStorageModule',
+      'ngMaterial',
+      'ngTable',
+      'ngDialog',
+      'angular-loading-bar',
+      'pascalprecht.translate'
+    ]);
+
+
+})();
