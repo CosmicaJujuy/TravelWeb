43f6783de7048061786bd4aede68bd919a81ca94
PauloGaldo <hades_54st@hotmail.com>
2016-10-26 02:06:52 -0300

    configuracion de las rutas agregada

---
+++ app/scripts/app.url.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    angular
+            .module('travelApp')
+            .config(configApp);
+
+    configApp.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider'];
+    /* @ngInject */
+    function configApp($stateProvider, $urlRouterProvider, $locationProvider) {
+
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+                .state('main', {
+                    url: '/',
+                    controller: 'MainCtrl as vm',
+                    templateUrl: 'views/main.html'
+                });
+
+    }
+
+})();
+
