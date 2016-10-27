c833e925fcd98abe1363674dd55fc09b18dd624a
PauloGaldo <hades_54st@hotmail.com>
2016-10-26 23:32:02 -0300

    instancia del server corregida

--- web.js
+++ web.js
@@ -1,5 +1,8 @@
-var express = require('express'),
-app = express();
-app.use(express.static(__dirname + '/dist'));
+var express = require('express');
+var appExp = express();
+var path = require('path');
 
-var server = app.listen(process.env.PORT || 443);
+appExp.use(express.static(__dirname));
+appExp.use(express.static(path.join(__dirname, '/dist')));
+
+var server = app.listen(process.env.PORT || 80);
